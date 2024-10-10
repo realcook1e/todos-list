9a12b16820ae9dd5f4287e871daaf2bccaaa244e
Cookie <eg.melnikk2@gmail.com>
2024-10-11 01:55:50 +0300

    deployment changes

--- src/tests/App.spec.tsx
+++ src/tests/App.spec.tsx
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import { expect, test } from "vitest";
 import { render } from "vitest-browser-react";
 import App from "../App";
